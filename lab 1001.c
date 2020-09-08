4a2ecd965940933f5e3009aa07419f608d2f53e3
nopporn01 <69183322+nopporn01@users.noreply.github.com>
2020-09-08 12:04:38 +0700

    Add files via upload

---
+++ lab 1001.c
@@ -0,0 +1,17 @@
+/*
+   Program:Operator
+   Authority:Nopporn
+   Date:08/09/2560
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+#include"mass.h"
+int main(){
+    hello_message();
+    operator1();
+    operator2();
+    operator3();
+     return 0;
+}
---
+++ mass.h
@@ -0,0 +1,8 @@
+#ifndef MASS_H_INCLUDED
+#define MASS_H_INCLUDED
+
+void hello_message();
+int operator1();
+int operator2();
+int operator3();
+#endif // MASS_H_INCLUDED
